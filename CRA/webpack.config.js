1f25400da06112d00ef0c244fe09d83a0f04c435
KYUTAE PARK <rbxo0109@gmail.com>
2023-01-21 22:05:18 +0900

    feat. CRA 해부해보기

--- CRA/webpack.config.js
+++ CRA/webpack.config.js
@@ -2,14 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'developer',
+    mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        static: './public'
+    },
     Plugin: [
         new HtmlWebpackPlugin({
         })
     ],
     output: {
-        filename: '',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'public'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 }
